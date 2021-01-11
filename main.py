00a68a3ce394abf5cf68eb5b94d591fd698ede72
GamerTime69420 <58784345+GamerTime69420@users.noreply.github.com>
2021-01-11 10:17:12 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,55 +1,57 @@
-input.on_button_pressed(Button.A, bruh)
-input.on_button_pressed(Button.B, jump)
-def mans():
-    led.plot(0,2)
-    led.plot(0,3)
 def bruh():
-    led.unplot(0,2)
-    basic.pause(800)
-    led.plot(0,2)
+    led.unplot(0, 2)
+    basic.pause(1000)
+    led.plot(0, 2)
+input.on_button_pressed(Button.A, bruh)
+
 def jump():
-    led.unplot(0,3)
-    led.plot(0,1)
-    basic.pause(800)
-    led.plot(0,3)
-    led.unplot(0,1)
+    led.unplot(0, 3)
+    led.plot(0, 1)
+    basic.pause(1000)
+    led.plot(0, 3)
+    led.unplot(0, 1)
+input.on_button_pressed(Button.B, jump)
+
+def mans():
+    led.plot(0, 2)
+    led.plot(0, 3)
 bbruh = 4
 def wall():
-    global bbruh
+    global bbruh, gameover
     for i in range(5):
-        led.plot(bbruh,0)
-        led.plot(bbruh,1)
-        led.plot(bbruh,2)
-        if led.point(bbruh - 1,2) == True:
-            gameover = True
+        led.plot(bbruh, 0)
+        led.plot(bbruh, 1)
+        led.plot(bbruh, 2)
         basic.pause(500)
-        led.unplot(bbruh,0)
-        led.unplot(bbruh,1)
-        led.unplot(bbruh,2)
+        if led.point(bbruh - 1, 2) == True:
+            gameover = True
+        led.unplot(bbruh, 0)
+        led.unplot(bbruh, 1)
+        led.unplot(bbruh, 2)
         bbruh = bbruh - 1
 def yeet():
-    global bbruh
+    global bbruh, gameover
     for i in range(5):
-        led.plot(bbruh,0)
-        led.plot(bbruh,3)
-        if led.point(bbruh - 1,3) == True:
-            gameover = True
+        led.plot(bbruh, 0)
+        led.plot(bbruh, 3)
         basic.pause(500)
-        led.unplot(bbruh,0)
-        led.unplot(bbruh,3)
+        if led.point(bbruh - 1, 3) == True:
+            gameover = True
+        led.unplot(bbruh, 0)
+        led.unplot(bbruh, 3)
         bbruh = bbruh - 1
 def floor():
-    led.plot(0,4)
-    led.plot(1,4)
-    led.plot(2,4)
-    led.plot(3,4)
-    led.plot(4,4)
-obsticals = [wall(), yeet()]
+    led.plot(0, 4)
+    led.plot(1, 4)
+    led.plot(2, 4)
+    led.plot(3, 4)
+    led.plot(4, 4)
+floor()
+mans()
 gameover = False
-
 while gameover == False:
-    x = randint(0,2)
-    floor()
-    mans()
-    obsticals[x]
-    basic.pause(2500)
+    x = randint(0, 1)
+    if x == 1:
+        wall()
+    else:
+        yeet()
--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 let x: number;
 input.onButtonPressed(Button.A, function bruh() {
     led.unplot(0, 2)
-    basic.pause(800)
+    basic.pause(1000)
     led.plot(0, 2)
 })
 input.onButtonPressed(Button.B, function jump() {
     led.unplot(0, 3)
     led.plot(0, 1)
-    basic.pause(800)
+    basic.pause(1000)
     led.plot(0, 3)
     led.unplot(0, 1)
 })
@@ -18,17 +18,16 @@ function mans() {
 
 let bbruh = 4
 function wall() {
-    let gameover: boolean;
     
     for (let i = 0; i < 5; i++) {
         led.plot(bbruh, 0)
         led.plot(bbruh, 1)
         led.plot(bbruh, 2)
+        basic.pause(500)
         if (led.point(bbruh - 1, 2) == true) {
             gameover = true
         }
         
-        basic.pause(500)
         led.unplot(bbruh, 0)
         led.unplot(bbruh, 1)
         led.unplot(bbruh, 2)
@@ -37,16 +36,15 @@ function wall() {
 }
 
 function yeet() {
-    let gameover: boolean;
     
     for (let i = 0; i < 5; i++) {
         led.plot(bbruh, 0)
         led.plot(bbruh, 3)
+        basic.pause(500)
         if (led.point(bbruh - 1, 3) == true) {
             gameover = true
         }
         
-        basic.pause(500)
         led.unplot(bbruh, 0)
         led.unplot(bbruh, 3)
         bbruh = bbruh - 1
@@ -61,12 +59,15 @@ function floor() {
     led.plot(4, 4)
 }
 
-let obsticals = [wall(), yeet()]
+floor()
+mans()
 let gameover = false
 while (gameover == false) {
-    x = randint(0, 2)
-    floor()
-    mans()
-    obsticals[x]
-    basic.pause(2500)
+    x = randint(0, 1)
+    if (x == 1) {
+        wall()
+    } else {
+        yeet()
+    }
+    
 }
